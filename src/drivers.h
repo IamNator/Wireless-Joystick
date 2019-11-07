02ec604a3367bcfa0bd1fa3a00cb7dae0fc842e1
grey-gray <natverior1@gmail.com>
2019-11-07 03:41:35 +0100

    Removing c files

--- src/drivers.h
+++
@@ -1,7 +0,0 @@
-#ifndef DRIVER_H
-#define DRIVER_H
-
-
-void ADC_init();
-
-#endif //DRIVER_H
--- src/functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCTIONS_H
-#def FUNCTIONS_H
-
-uint16_t ADC_read(uint8_t chnl);
-void Send(uint16_t AnalogData);
-
-
-#endif FUNCTIONS_H
